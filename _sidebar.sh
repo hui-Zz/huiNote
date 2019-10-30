1a9ecbec0f1615dc7a50be0a84a6b97e5d3daf27
hui-Zz <hui0.0713@gmail.com>
2019-10-30 20:21:38 +0800

    侧边栏生成所有层级目录

--- _sidebar.sh
+++ _sidebar.sh
@@ -1,35 +1,29 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "- [返回](/README)" >Apps/_sidebar.md
-find ./Apps -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>Apps/_sidebar.md
+function getdir(){
+    for files in `ls $1`
+    do
+        full_path=$1"/"$files
+        file_path=$1
+        file_name=$f
+#如果是文件的话，则文件名和后缀相同，故在此过滤
+        if [[ -f ${full_path} ]] && [[ ${files##*.} == "md" ]] && [[ ${files%.*} != "_sidebar" ]]; then
+            #arr[$file_path]=arr[$file_path]"\n- ["${files%.*}"]("${file_path/$(pwd)/}"/"${files%.*}")"
+            echo "- ["${files%.*}"]("${file_path/$(pwd)/}"/"${files%.*}")">>${file_path}/_sidebar.md
+            #echo "filepath:" ${file_path}
+            #echo "filename: ${files%.*}"
+            #echo "extension: ${files##*.}"
+        fi
+#如果发现文件夹下还有子文件夹，进行递归遍历
+        if [[ -d ${full_path} ]] && [[ ${files} != "assets" ]]; then
+            #echo "- "${files}>>_sidebar.md
+            dir_path=${full_path/$(pwd)/}
+            echo "- [上一层]("${dir_path/${files}/}")">${full_path}/_sidebar.md
+            getdir ${full_path}
+        fi
+        #echo "------split------"
+    done
+}
 
-echo "- [返回](/README)" >DB/_sidebar.md
-find ./DB -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>DB/_sidebar.md
-
-echo "- [返回](/README)" >Geek/_sidebar.md
-find ./Geek -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>Geek/_sidebar.md
-
-echo "- [返回](/README)" >Java/_sidebar.md
-find ./Java -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>Java/_sidebar.md
-
-echo "- [返回](/README)" >JavaEE/_sidebar.md
-find ./JavaEE -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>JavaEE/_sidebar.md
-
-echo "- [返回](/README)" >Mobile/_sidebar.md
-find ./Mobile -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>Mobile/_sidebar.md
-
-echo "- [返回](/README)" >Study/_sidebar.md
-find ./Study -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>Study/_sidebar.md
-
-echo "- [返回](/README)" >Sys/_sidebar.md
-find ./Sys -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>Sys/_sidebar.md
-
-echo "- [返回](/README)" >Think/_sidebar.md
-find ./Think -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>Think/_sidebar.md
-
-echo "- [返回](/README)" >UI/_sidebar.md
-find ./UI -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>UI/_sidebar.md
-
-echo "- [返回](/README)" >Work/_sidebar.md
-find ./Work -mindepth 2 -name "*.md" | awk -F'/' 'BEGIN {RS=".md"} {arr[$2]=arr[$2]"\n    - ["$3"](/"$2"/"$3")"} END { num = asorti(arr, indices); for (i=1; i<=num; ++i) if (indices[i]) print "- "indices[i], arr[indices[i]]}' >>Work/_sidebar.md
-
+folder_path=$(pwd)
+getdir ${folder_path}
