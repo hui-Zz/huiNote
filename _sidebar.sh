d3f9a707a2c4d7ae9dd91287ccabddbf8ac3ed8e
hui-Zz <hui0.0713@gmail.com>
2019-10-31 14:02:04 +0800

    在子目录的侧边栏生成返回上几级目录

--- _sidebar.sh
+++ _sidebar.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-function getdir(){
-    for files in `ls $1`
-    do
+function getdir() {
+    for files in $(ls $1); do
         full_path=$1"/"$files
         file_path=$1
         file_name=$f
-#如果是文件的话，则文件名和后缀相同，故在此过滤
+        #如果是文件的话，在此过滤
         if [[ -f ${full_path} ]] && [[ ${files##*.} == "md" ]] && [[ ${files} != "_sidebar.md" ]] && [[ ${files} != "_coverpage.md" ]] && [[ ${files} != "README.md" ]] && [[ ${files} != "SUMMARY.md" ]]; then
             #arr[$file_path]=arr[$file_path]"\n- ["${files%.*}"]("${file_path/$(pwd)/}"/"${files%.*}")"
-            echo "- ["${files%.*}"]("${file_path/$(pwd)/}"/"${files%.*}")">>${file_path}/_sidebar.md
+            echo "- ["${files%.*}"]("${file_path/$(pwd)/}"/"${files%.*}")" >>${file_path}/_sidebar.md
             #echo "filepath:" ${file_path}
             #echo "filename: ${files%.*}"
             #echo "extension: ${files##*.}"
         fi
-#如果发现文件夹下还有子文件夹，进行递归遍历
+        #如果发现文件夹下还有子文件夹，进行递归遍历
         if [[ -d ${full_path} ]] && [[ ${files} != "assets" ]]; then
-            dir_path=${full_path/$(pwd)/}
-            echo "- ["${files}"]("${dir_path}"/)">>${file_path}/_sidebar.md
-            echo "- [[上一层]]("${dir_path/${files}/}")">${full_path}/_sidebar.md
+            # 生成每个目录默认README.md页面
+            echo "# "${files} >${full_path}/README.md
+            # 生成嵌套的侧边结构文件
+            current_relative_path=${full_path/$(pwd)/}
+            echo "- ["${files}"]("${current_relative_path}"/)" >>${file_path}/_sidebar.md
+            parent_relative_path=${current_relative_path/${files}/}
+
+            echo "- [[目录]](/)" >${full_path}/_sidebar.md
+            # 分割路径到数组
+            array=(${parent_relative_path//// })
+            str=""
+            for ((i = 0; i < ${#array[@]}; i++)); do
+                str=${str}"/"${array[i]}
+                echo "- [["${str}"]]("${str}"/)" >>${full_path}/_sidebar.md
+            done
+
             getdir ${full_path}
         fi
         #echo "------split------"
@@ -26,6 +38,6 @@ function getdir(){
 }
 
 folder_path=$(pwd)
-echo "">${folder_path}/_sidebar.md
+echo "" >${folder_path}/_sidebar.md
 getdir ${folder_path}
-echo "- [关于](README)">>${folder_path}/_sidebar.md
+echo "- [关于](README)" >>${folder_path}/_sidebar.md
